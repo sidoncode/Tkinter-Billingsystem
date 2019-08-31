7a4f2461e23ea8d369fe2239ccd3f4a6fa6567e8
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-08-31 22:06:50 +0530

    started the cost value in thealgo

--- app.py
+++ app.py
@@ -42,3 +42,19 @@ def costFieldListener(a,b,c):
     else:
         cost=0
         costVar.set(cost)
+#============= global variable for  entries===========
+#========login variable========
+usernameVar = StringVar()
+passwordVar = StringVar()
+
+#============main window variable=======
+options=[]
+rateDict={}
+itemVariable=StringVar()
+quantityVar = StringVar()
+quantityVar.trace('w',quantityFieldListener)
+itemRate=2
+rateVar = StringVar()
+rateVar.set("%.2f"%itemRate)
+costVar=StringVar()
+costVar.trace('w', costFieldListener)
