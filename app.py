9af3357e7b86bff2cee5f2912a57f2ae250289ca
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-08-31 22:05:03 +0530

    changed the algo

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ import pymysql
 window = Tk()
 window.geometry("900x600")
 window.title("Billing")
+#===========field listner ==========================
+def quantityFieldListener(a,b,c):
+    global quantityVar
+    global costVar
+    global itemRate
+    quantity = quantityVar.get()
+    if quantity != "":
+        try:
+            quantity=float(quantity)
+            cost = quantity*itemRate
+            quantityVar.set("%.2f"%quantity)
+            costVar.set("%.2f"%cost)
+        except ValueError:
+            quantity=quantity[:-1]
+            quantityVar.set(quantity)
+    else:
+        quantity=0
+        quantityVar.set("%.2f"%quantity)
