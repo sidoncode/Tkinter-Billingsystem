865b882f6b60260a884f99275b85abe3321d211c
Siddharth Sharma <siddharth54330@gmail.com>
2020-03-11 20:23:07 +0530

    Add files via upload
    new file commit

---
+++ 116A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+    int n, a, b, c(0), m(0);
+    scanf("%d", &n);
+    while (n--){
+        scanf("%d%d", &a, &b);
+        c -= a;
+        c += b;
+        if (c > m){
+            m = c;
+        }
+    }
+    printf("%d\n", m);
+}
